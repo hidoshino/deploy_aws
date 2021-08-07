363eb0cacb3e6c267bfeeb746e009f83e0319c39
hidoshino <83619908+hidoshino@users.noreply.github.com>
2021-08-07 08:47:44 -0300

    cors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "localhost:3000",
+		origin: "https://d31ftre6ylgggh.cloudfront.net/",
 		methods: [ "GET", "POST" ]
 	}
 })
